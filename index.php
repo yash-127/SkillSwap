27dfd26ee828b3e9285ca69442dcbb33149abb76
Yash Joshi <yashjoshi2050@gmail.com>
2025-10-04 13:32:07 +0530

    Adding header and css for body and header, making script.js

--- index.php
+++ index.php
@@ -10,6 +10,23 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
-    
+    <header>
+        <nav class="container">
+            <a href="/" class="logo nav-link">SkillSwap ✨</a>
+            <div class="nav-links">
+                <a href="/" class="nav-link">Browse</a>
+                <a href="/post" class="nav-link requires-auth">Post a Task</a>
+            </div>
+            <div class="nav-auth" id="nav-auth-logged-out">
+                <a href="/login" class="login-btn nav-link">Login</a>
+                <a href="/login" class="signup-btn nav-link" data-target-signup="true">Sign Up</a>
+            </div>
+            <div class="nav-auth hidden" id="nav-auth-logged-in">
+                 <span class="welcome-user">Welcome, User!</span>
+                <a href="#" id="logout-btn" class="logout-btn">Logout</a>
+            </div>
+        </nav>
+    </header>
+
 </body>
 </html>
